5e4abca86a2e80b8e03b672f3a40f50b457389f2
Oleh Kuzyk <o.kuzyk.2001@gmail.com>
2018-11-23 16:33:45 +0200

    ListOfCafes clean-coded

--- app/src/main/java/ua/com/mnbs/noq/ListOfCafes.java
+++ app/src/main/java/ua/com/mnbs/noq/ListOfCafes.java
@@ -17,91 +17,73 @@ public class ListOfCafes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_cafes);
-        ArrayList<String> Name = new ArrayList<>();
-        ArrayList<String> Location = new ArrayList<>();
-        ArrayList<String> Type = new ArrayList<>();
-        String text = "";
-        try {
-            InputStream inputStream = getAssets().open("cafe_names.txt");
-            int size = inputStream.available();
-            byte[] buffer = new byte[size];
-            inputStream.read(buffer);
-            inputStream.close();
-            text = new String(buffer);
-            text += '\0';
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String temp = "";
-        for (int i = 0; i < text.length(); i++)
-        {
-            if (text.charAt(i) == '\n' || text.charAt(i) == '\0') {
-                Name.add(temp);
-                temp = "";
-                continue;
-            }
-            temp += text.charAt(i);
-        }
 
-        text = "";
-        try {
-            InputStream inputStream = getAssets().open("cafe_locations.txt");
-            int size = inputStream.available();
-            byte[] buffer = new byte[size];
-            inputStream.read(buffer);
-            inputStream.close();
-            text = new String(buffer);
-            text += '\0';
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        temp = "";
-        for (int i = 0; i < text.length(); i++)
-        {
-            if (text.charAt(i) == '\n' || text.charAt(i) == '\0') {
-                Location.add(temp);
-                temp = "";
-                continue;
-            }
-            temp += text.charAt(i);
-        }
+        String names = readFile("cafe_names.txt");
+        String locations = readFile("cafe_locations.txt");
+        String types = readFile("cafe_types.txt");
 
-        text = "";
-        try {
-            InputStream inputStream = getAssets().open("cafe_types.txt");
-            int size = inputStream.available();
-            byte[] buffer = new byte[size];
-            inputStream.read(buffer);
-            inputStream.close();
-            text = new String(buffer);
-            text += '\0';
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        temp = "";
-        for (int i = 0; i < text.length(); i++)
-        {
-            if (text.charAt(i) == '\n' || text.charAt(i) == '\0') {
-                Type.add(temp);
-                temp = "";
-                continue;
-            }
-            temp += text.charAt(i);
-        }
+        ArrayList<String> name = moveIntoArrayList(names);
+        ArrayList<String> location = moveIntoArrayList(locations);
+        ArrayList<String> type = moveIntoArrayList(types);
 
         ArrayList<Cafe> cafes = new ArrayList<>();
-        if (Name.size() != Location.size() || Name.size() != Type.size() || Location.size() != Type.size())
-            Toast.makeText(getApplicationContext(),"Something is wrong with your text files.",
+
+        if (isMistakeInFiles(name, location, type))
+            Toast.makeText(getApplicationContext(), "Something is wrong with your text files.",
                     Toast.LENGTH_SHORT).show();
-       else {
-            for (int i = 0; i < Name.size(); i++)
-                cafes.add(new Cafe(Name.get(i), Location.get(i), Type.get(i)));
+        else {
+            cafes = createCafeArrayList(name, location, type);
+            printListOfCafes(cafes);
+        }
 
 
-                CafeAdapter adapter = new CafeAdapter(this, cafes);
-                ListView listView = (ListView) findViewById(R.id.cafe_list);
-                listView.setAdapter(adapter);
+    }
+
+    private String readFile(String fileName) {
+        String text = "";
+        try {
+            InputStream inputStream = getAssets().open(fileName);
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            text = new String(buffer);
+            text += '\0';
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
+    }
+
+    private ArrayList<String> moveIntoArrayList(String text) {
+        ArrayList<String> returnValue = new ArrayList<>();
+        String temp = "";
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == '\n' || text.charAt(i) == '\0') {
+                returnValue.add(temp);
+                temp = "";
+                continue;
             }
+            temp += text.charAt(i);
+        }
+        return returnValue;
+    }
 
+    private void printListOfCafes(ArrayList<Cafe> cafes) {
+        CafeAdapter adapter = new CafeAdapter(this, cafes);
+        ListView listView = (ListView) findViewById(R.id.cafe_list);
+        listView.setAdapter(adapter);
+    }
+
+
+    private boolean isMistakeInFiles(ArrayList<String> name, ArrayList<String> location, ArrayList<String> type) {
+        return (name.size() != location.size() || name.size() != type.size() || location.size() != type.size());
+    }
+
+    private ArrayList<Cafe> createCafeArrayList(ArrayList<String> name, ArrayList<String> location, ArrayList<String> type) {
+        ArrayList<Cafe> cafes = new ArrayList<>();
+        for (int i = 0; i < name.size(); i++)
+            cafes.add(new Cafe(name.get(i), location.get(i), type.get(i)));
+        return cafes;
     }
 }
