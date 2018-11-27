2fb7f0d47ebbd0122eeb55d346b77a84ac6871ba
Олег Падучак <padychak.o@gmail.com>
2018-11-27 22:55:44 +0200

    Begun creating forming Order file(contains only cafe info

--- app/src/main/java/ua/com/mnbs/noq/ListOfCafes.java
+++ app/src/main/java/ua/com/mnbs/noq/ListOfCafes.java
@@ -1,5 +1,6 @@
 package ua.com.mnbs.noq;
 
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.media.AudioManager;
@@ -15,11 +16,15 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
 public class ListOfCafes extends AppCompatActivity {
+
+    ArrayList<Cafe> cafes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +35,9 @@ public class ListOfCafes extends AppCompatActivity {
         String locations = readFile("cafe_locations.txt");
         String types = readFile("cafe_types.txt");
 
-        ArrayList<String> name = moveIntoArrayList(names);
+        final ArrayList<String> name = moveIntoArrayList(names);
         ArrayList<String> location = moveIntoArrayList(locations);
         ArrayList<String> type = moveIntoArrayList(types);
-
-        ArrayList<Cafe> cafes = new ArrayList<>();
 
         if (isMistakeInFiles(name, location, type))
             Toast.makeText(getApplicationContext(), "Something is wrong with your text files.",
@@ -42,21 +45,27 @@ public class ListOfCafes extends AppCompatActivity {
         else {
             cafes = createCafeArrayList(name, location, type);
             printListOfCafes(cafes);
-        }
 
-        ListView listView = (ListView) findViewById(R.id.cafe_list);
+            ListView listView = (ListView) findViewById(R.id.cafe_list);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapter, View view, int position, long l) {
-                if (position == 0) {
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapter, View view, int position, long l) {
+
                     Intent OpenMenu = new Intent(ListOfCafes.this, ListOfMeals.class);
+                    OpenMenu.putExtra("cafe name", cafes.get(position).getCafeName());
+                    OpenMenu.putExtra("position", position);
+                    String k = makeNewOrderFileName(ReadFromFileNotAsset("counter.txt"));
+                    deleteFile("counter.txt");
+                    WriteToFile("counter.txt",k);
+                    WriteToFile("Order"+getStringFileSize("counter.txt"),cafes.get(position).getCafeName()+"\n"+cafes.get(position).getCafeType()+"\n"+cafes.get(position).getCafeLocation());
                     startActivity(OpenMenu);
                 }
-            }
-        });
+            });
+        }
 
     }
+
 
     private String readFile(String fileName) {
         String text = "";
@@ -73,6 +82,54 @@ public class ListOfCafes extends AppCompatActivity {
         }
         return text;
     }
+    public String ReadFromFileNotAsset(String file){
+        String text= "";
+        try{
+            FileInputStream fis = openFileInput(file);
+            int size = fis.available();
+            byte[] buffer = new byte[size];
+            fis.read(buffer);
+            fis.close();
+            text = new String(buffer);
+        }
+        catch (IOException e)
+        {
+            Toast.makeText(ListOfCafes.this,"Error Reading from file",Toast.LENGTH_SHORT).show();
+        }
+        return text;
+
+    }
+
+
+    public boolean counterIsEmpty(){
+        boolean empty= false;
+        try{
+            FileInputStream fis = openFileInput("counter.txt");
+            int size = fis.available();
+            if (size==0){
+                empty=true;
+            }
+            fis.close();
+        }
+        catch (IOException e)
+        {
+            Toast.makeText(ListOfCafes.this,"Error Reading from file",Toast.LENGTH_SHORT).show();
+        }
+        return empty;
+
+    }
+
+    public String makeNewOrderFileName(String text){
+        if (counterIsEmpty()==true){
+            return "0";
+        }
+        else {
+            int counter = Integer.parseInt(text);
+            counter++;
+            String smth = Integer.toString(counter);
+            return smth;
+        }
+    }
 
     private ArrayList<String> moveIntoArrayList(String text) {
         ArrayList<String> returnValue = new ArrayList<>();
@@ -87,6 +144,28 @@ public class ListOfCafes extends AppCompatActivity {
         }
         return returnValue;
     }
+
+    protected void WriteToFile(String file, String text)
+    {
+        try {
+            FileOutputStream fos = openFileOutput(file,Context.MODE_PRIVATE);
+            fos.write(text.getBytes());
+            fos.close();
+        }
+        catch (IOException e){
+            Toast.makeText(ListOfCafes.this,"Error Writing to file",Toast.LENGTH_SHORT).show();
+        }
+    }
+    private int getStringFileSize(String file){
+        int size=0;
+        try {
+            FileInputStream fis = openFileInput(file);
+            size=fis.available();
+        }
+        catch (IOException e){}
+        return size;
+    }
+
 
     private void printListOfCafes(ArrayList<Cafe> cafes) {
         CafeAdapter adapter = new CafeAdapter(this, cafes);
