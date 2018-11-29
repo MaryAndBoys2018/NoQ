cba787264739f51ad5d4d6ba12da18c090ee8a49
Maxym Marina <maxymmrn@gmail.com>
2018-11-29 15:49:51 +0200

    upgrade TimeActivity and added intent to activity_my_order

--- app/src/main/java/ua/com/mnbs/noq/TimeActivity.java
+++ app/src/main/java/ua/com/mnbs/noq/TimeActivity.java
@@ -25,32 +25,63 @@ public class TimeActivity extends AppCompatActivity {
         currentTime = (TextView) findViewById(R.id.current_time);
         orderTime = (TextView) findViewById(R.id.text_time);
 
-        currentTime.setText(" " + getTime());
+        currentTime.setText(" " + updateDisplay());
+
+        floatTime.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                updateDisplay(hourOfDay, minute);
+            }
+        });
 
         submitTime = (Button) findViewById(R.id.submit_time);
 
         submitTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                orderTime.setText(" " + getTime());
+                Intent OpenListOfOrders = new Intent(TimeActivity.this, activity_my_orders.class);
+                startActivity(OpenListOfOrders);
             }
         });
     }
 
-    private String getTime(){
+    private void updateDisplay(int hour, int minute){
+        Integer orderHour = hour;
+        Integer orderMinute = minute;
+
+        String mOrderTime = convertTime(orderHour, orderMinute);
+
+        orderTime.setText(" " + mOrderTime);
+    }
+
+    private String updateDisplay(){
 
         Integer orderHour = floatTime.getHour();
         Integer orderMinute = floatTime.getMinute();
 
-        String mOrderTime = orderHour.toString();
-        mOrderTime += ":";
-
-        if (orderMinute < 10){
-            mOrderTime += "0";
-        }
-
-        mOrderTime += orderMinute.toString();
+        String mOrderTime = convertTime(orderHour, orderMinute);
 
         return  mOrderTime;
+    }
+
+    private String fixZero(Integer num){
+        String stringNum;
+
+        if (num < 10){
+            stringNum = "0";
+            stringNum += num.toString();
+        }else{
+            stringNum = num.toString();
+        }
+
+        return stringNum;
+    }
+
+    private String convertTime(Integer hour, Integer minute){
+        String convertedTime = fixZero(hour);
+        convertedTime += ":";
+        convertedTime += fixZero(minute);
+
+        return  convertedTime;
     }
 }
