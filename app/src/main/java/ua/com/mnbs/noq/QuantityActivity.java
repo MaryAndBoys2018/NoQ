adbe6597b5ec97927f3f98e0a77f74e27a263f29
Maxym Marina <maxymmrn@gmail.com>
2018-12-01 16:07:25 +0200

    added version of code

--- app/src/main/java/ua/com/mnbs/noq/QuantityActivity.java
+++ app/src/main/java/ua/com/mnbs/noq/QuantityActivity.java
@@ -1,19 +1,57 @@
 package ua.com.mnbs.noq;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 public class QuantityActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quantity);
+        setContentView(R.layout.activity_menu_quantity);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String currentCafe = extras.getString("cafe name");
+        int position = extras.getInt("position");
+
+        currentCafe = currentCafe.trim();
+
+        if (position == 0)
+            currentCafe = currentCafe.substring(1,currentCafe.length());
+
+        String menuFileDirectory = currentCafe + "_menu.txt";
+        String pricesFileDirectory = currentCafe + "_prices.txt";
+
+        String names = readFile(menuFileDirectory);
+
+        ArrayList<String> name = moveIntoArrayList(names);
+        String prices = readFile(pricesFileDirectory);
+        ArrayList<String> price = moveIntoArrayList(prices);
+
+        ArrayList<Meal> meals = new ArrayList<>();
+
+        if (isMistakeInFiles(name, price))
+            Toast.makeText(getApplicationContext(), "Something is wrong with your text files.",
+                    Toast.LENGTH_SHORT).show();
+        else {
+            meals = createMealArrayList(name, price);
+            printListOfMeals(meals);
+        }
         Button btn = (Button) findViewById(R.id.btn1);
 
         btn.setOnClickListener(new View.OnClickListener() {
@@ -47,5 +85,53 @@ public class QuantityActivity extends AppCompatActivity {
     private void displayQuantity(int numberOfCoffees) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + numberOfCoffees);
+    }
+
+    private String readFile(String fileName) {
+        String text = "";
+        try {
+            InputStream inputStream = getAssets().open(fileName);
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            text = new String(buffer);
+            text += '\0';
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
+    }
+
+    private ArrayList<String> moveIntoArrayList(String text) {
+        ArrayList<String> returnValue = new ArrayList<>();
+        String temp = "";
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == '\n' || text.charAt(i) == '\0') {
+                returnValue.add(temp);
+                temp = "";
+                continue;
+            }
+            temp += text.charAt(i);
+        }
+        return returnValue;
+    }
+
+
+    private void printListOfMeals(ArrayList<Meal> meals) {
+        MenuAdapter adapter = new MenuAdapter(this, meals);
+        ListView listView = (ListView) findViewById(R.id.menu_list);
+        listView.setAdapter(adapter);
+    }
+
+    private boolean isMistakeInFiles(ArrayList<String> name, ArrayList<String> price) {
+        return (name.size() != price.size());
+    }
+
+    private ArrayList<Meal> createMealArrayList(ArrayList<String> name, ArrayList<String> price) {
+        ArrayList<Meal> meals = new ArrayList<>();
+        for (int i = 0; i < name.size(); i++)
+            meals.add(new Meal(name.get(i), price.get(i)));
+        return meals;
     }
 }
---
+++ app/src/main/java/ua/com/mnbs/noq/menu_quantity.java
@@ -0,0 +1,35 @@
+package ua.com.mnbs.noq;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class menu_quantity extends ArrayAdapter<Meal> {
+    menu_quantity(Activity context, ArrayList<Meal> meals) {
+        super(context, 0, meals);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.activity_quantity, parent, false);
+        }
+        Meal currentMeal = getItem(position);
+
+
+        TextView mealNameTextView = (TextView) listItemView.findViewById(R.id.meal_name_text_view);
+        mealNameTextView.setText(currentMeal.getMealName());
+
+        TextView priceTypeTextView = (TextView) listItemView.findViewById(R.id.price_type_text_view);
+        priceTypeTextView.setText(currentMeal.getMealPrice());
+        return listItemView;
+    }
+
+}
