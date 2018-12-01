c74a3123fd129ddb3154969126da1437f1ae2f86
olehzherebukh <olehzherebukh@gmail.com>
2018-12-01 21:42:50 +0200

    working QuantityActivity

--- app/src/main/java/ua/com/mnbs/noq/QuantityActivity.java
+++ app/src/main/java/ua/com/mnbs/noq/QuantityActivity.java
@@ -24,7 +24,11 @@ public class QuantityActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_quantity);
 
-
+        final ArrayList<Meal> meals = new ArrayList<Meal>();
+        meals.add(new Meal("Гамбургер", "40грн"));
+        QuantityAdapter adapter = new QuantityAdapter( this, meals);
+        ListView listView = (ListView) findViewById(R.id.menu_list);
+        listView.setAdapter(adapter);
 
 
 
@@ -63,51 +67,5 @@ public class QuantityActivity extends AppCompatActivity {
         quantityTextView.setText("" + numberOfCoffees);
     }
 
-    private String readFile(String fileName) {
-        String text = "";
-        try {
-            InputStream inputStream = getAssets().open(fileName);
-            int size = inputStream.available();
-            byte[] buffer = new byte[size];
-            inputStream.read(buffer);
-            inputStream.close();
-            text = new String(buffer);
-            text += '\0';
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return text;
-    }
 
-    private ArrayList<String> moveIntoArrayList(String text) {
-        ArrayList<String> returnValue = new ArrayList<>();
-        String temp = "";
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == '\n' || text.charAt(i) == '\0') {
-                returnValue.add(temp);
-                temp = "";
-                continue;
-            }
-            temp += text.charAt(i);
-        }
-        return returnValue;
-    }
-
-
-    private void printListOfMeals(ArrayList<Meal> meals) {
-        MenuAdapter adapter = new MenuAdapter(this, meals);
-        ListView listView = (ListView) findViewById(R.id.menu_list);
-        listView.setAdapter(adapter);
-    }
-
-    private boolean isMistakeInFiles(ArrayList<String> name, ArrayList<String> price) {
-        return (name.size() != price.size());
-    }
-
-    private ArrayList<Meal> createMealArrayList(ArrayList<String> name, ArrayList<String> price) {
-        ArrayList<Meal> meals = new ArrayList<>();
-        for (int i = 0; i < name.size(); i++)
-            meals.add(new Meal(name.get(i), price.get(i)));
-        return meals;
-    }
 }
