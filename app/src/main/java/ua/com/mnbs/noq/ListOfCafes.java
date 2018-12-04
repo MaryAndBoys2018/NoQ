016363a884672b8bbee6bb67ddb83b4c7566c6ce
Roman Spodaryk <roman.spodaryk@ukr.net>
2018-12-04 20:29:01 +0200

    create new cafes

--- app/src/main/java/ua/com/mnbs/noq/ListOfCafes.java
+++ app/src/main/java/ua/com/mnbs/noq/ListOfCafes.java
@@ -7,6 +7,7 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -85,6 +86,7 @@ public class ListOfCafes extends AppCompatActivity {
                 startActivity(toMainActivity);
             }
         });
+
 
     }
 
